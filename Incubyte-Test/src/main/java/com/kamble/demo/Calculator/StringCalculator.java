0dbe4df71f4bd590a7408bf3efb83c80f9256c76
Sanket Kamble <1997sanket@gmail.com>
2021-08-09 23:23:27 +0530

    Create Calculator class and Add method with basic logic

---
+++ Incubyte-Test/src/main/java/com/kamble/demo/Calculator/StringCalculator.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.kamble.demo.Calculator;
+
+/**
+ * @author Sanket Kamble
+ *
+ */
+public class StringCalculator {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringCalculator sc = new StringCalculator();
+		int sum = sc.Add("");
+
+	}
+
+	public int Add(String numbers) {
+
+		return 1;
+	}
+
+}
---
+++ Incubyte-Test/src/main/java/com/kamble/demo/IncubyteTestApplication.java
@@ -0,0 +1,13 @@
+package com.kamble.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubyteTestApplication.class, args);
+	}
+
+}
